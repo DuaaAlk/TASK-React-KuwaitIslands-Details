af46d3d5406103c6b7827fa3802b272caf97a48e
DuaaAlk <engduaa2013@gmail.com>
2022-01-21 12:23:50 +0300

    Last Commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 
+import { useState } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import IslandForm from './Components/IslandForm';
 import IslandList from './Components/IslandList';
-
+import data from './data/islands'
 
 function App() {
-
-  let island = {
-    name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
+  const [island, setIsland] = useState(data[0]);
 
   return (
-
-    <div>
-      <Header />
-
-      <div className="homePage">
-        <IslandList />
-        <IslandForm island={island} />
-      </div>
-    </div>
+          <div>
+            <Header />
+            <div className="homePage">
+              <IslandList setIsland = {setIsland}/>
+              <IslandForm island={island} />
+            </div>
+          </div>
   );
 }
 
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function IslandForm({ island }) {
+
+  const [phoneNumber, setPhoneNumber] = useState("Type Phone Number");
+  const [nameInput, setNameInput] = useState("Type Full Name");
+
+  function checkPhoneNumber (event){
+    setPhoneNumber(event.target.value);
+  }
+
+  function confirmReservation () {
+    window.confirm(`Are you sure you want to book to ${island.name} 
+        with the following creditionals.\n Name: ${nameInput}\n Phone: ${phoneNumber}`)
+  }
+
+  function nameInputHandler (event){
+    setNameInput(event.target.value);
+  }
+
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input  placeholder= {nameInput} onChange={nameInputHandler}/>
+      <input  placeholder= {phoneNumber} type="tel" onChange={checkPhoneNumber}/>
+      <button className="book" onClick={confirmReservation}>
         Book for today!
       </button>
     </div>
